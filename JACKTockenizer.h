b4d561a4182f0043b3cf9da9a8fdb413b71af430
Juan Linde <jslinde123@gmail.com>
2021-12-30 16:55:22 -0500

    Add project files.

---
+++ JACKTockenizer.h
@@ -0,0 +1,9 @@
+#pragma once
+/*
+	What it does: This class creates a template for tockenizer objects. Tockenizer objects
+	              receive a stream of characters from a JACK file and return an XML file with the
+				  tockens of the file organized according to an agreed upon template.
+*/
+class JACKTockenizer {
+
+};
